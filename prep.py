7b56d5d1164e179a6b4bf97789204ed987d5cfa9
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2016-11-21 00:21:54 +0100

    Updates

--- prep.py
+++ prep.py
@@ -7,23 +7,81 @@ from glob import glob
 if not os.path.exists('data'):
     os.mkdir('data')
 
+    
+import numpy as np
+import pandas as pd
+
+names = ['Alice', 'Bob', 'Charlie', 'Dan', 'Edith', 'Frank', 'George',
+'Hannah', 'Ingrid', 'Jerry', 'Kevin', 'Laura', 'Michael', 'Norbert', 'Oliver',
+'Patricia', 'Quinn', 'Ray', 'Sarah', 'Tim', 'Ursula', 'Victor', 'Wendy',
+'Xavier', 'Yvonne', 'Zelda']
+
+k = 100
+
+
+def account_params(k):
+    ids = np.arange(k, dtype=int)
+    names2 = np.random.choice(names, size=k, replace=True)
+    wealth_mag = np.random.exponential(100, size=k)
+    wealth_trend = np.random.normal(10, 10, size=k)
+    freq = np.random.exponential(size=k)
+    freq /= freq.sum()
+
+    return ids, names2, wealth_mag, wealth_trend, freq
+
+def account_entries(n, ids, names, wealth_mag, wealth_trend, freq):
+    indices = np.random.choice(ids, size=n, replace=True, p=freq)
+    amounts = ((np.random.normal(size=n) + wealth_trend[indices])
+                                         * wealth_mag[indices])
+
+    return pd.DataFrame({'id': indices,
+                         'names': names[indices],
+                         'amount': amounts.astype('i4')},
+                         columns=['id', 'names', 'amount'])
+
+
+def accounts(n, k):
+    ids, names, wealth_mag, wealth_trend, freq = account_params(k)
+    df = account_entries(n, ids, names, wealth_mag, wealth_trend, freq)
+    return df
+
+
+def json_entries(n, *args):
+    df = account_entries(n, *args)
+    g = df.groupby(df.id).groups
+
+    data = []
+    for k in g:
+        sub = df.iloc[g[k]]
+        d = dict(id=int(k), name=sub['names'].iloc[0],
+                transactions=[{'transaction-id': int(i), 'amount': int(a)}
+                              for i, a in list(zip(sub.index, sub.amount))])
+        data.append(d)
+
+    return data
+
+def accounts_json(n, k):
+    args = account_params(k)
+    return json_entries(n, *args)
+  
+    
+    
 
 def random_array():
-    if os.path.exists(os.path.join('data', 'random.hdf5')):
+    if os.path.exists(os.path.join('random.hdf5')):
         return
 
     print("Create random data for array exercise")
     import h5py
 
-    with h5py.File(os.path.join('data', 'random.hdf5')) as f:
+    with h5py.File(os.path.join('random.hdf5')) as f:
         dset = f.create_dataset('/x', shape=(1000000000,), dtype='f4')
         for i in range(0, 1000000000, 1000000):
             dset[i: i + 1000000] = np.random.exponential(size=1000000)
 
 
 def accounts_csvs(num_files, n, k):
-    from accounts import account_entries, account_params
-    fn = os.path.join('data', 'accounts.%d.csv' % (num_files - 1))
+    fn = os.path.join('accounts.%d.csv' % (num_files - 1))
 
     if os.path.exists(fn):
         return
@@ -34,12 +92,11 @@ def accounts_csvs(num_files, n, k):
 
     for i in range(num_files):
         df = account_entries(n, *args)
-        df.to_csv(os.path.join('data', 'accounts.%d.csv' % i),
+        df.to_csv(os.path.join('accounts.%d.csv' % i),
                   index=False)
 
 
 def accounts_json(num_files, n, k):
-    from accounts import account_params, json_entries
     import json
     import gzip
     fn = os.path.join('data', 'accounts.%02d.json.gz' % (num_files - 1))
@@ -58,10 +115,10 @@ def accounts_json(num_files, n, k):
 
 
 def create_weather(growth=3200):
-    filenames = sorted(glob(os.path.join('data', 'weather-small', '*.hdf5')))
+    filenames = sorted(glob('weather-small/*.hdf5'))
 
-    if not os.path.exists(os.path.join('data', 'weather-big')):
-        os.mkdir(os.path.join('data', 'weather-big'))
+    if not os.path.exists('weather-big'):
+        os.mkdir('weather-big')
 
     if all(os.path.exists(fn.replace('small', 'big')) for fn in filenames):
         return
@@ -77,7 +134,7 @@ def create_weather(growth=3200):
 
         y = imresize(x, growth)
 
-        out_fn = os.path.join('data', 'weather-big', os.path.split(fn)[-1])
+        out_fn = os.path.join('weather-big', os.path.split(fn)[-1])
 
         with h5py.File(out_fn) as f:
             f.create_dataset('/t2m', data=y, chunks=(500, 500))
